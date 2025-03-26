5cb7040ef1267c9e7ba5dec028708b1dce6fac82
BetaHydri <jan_tiedemann@hotmail.com>
2025-03-26 13:37:41 +0100

    New Version

--- obj/Release/net6.0/win-x64/GetADGroupMembersFSP.AssemblyInfo.cs
+++ obj/Release/net6.0/win-x64/GetADGroupMembersFSP.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GetADGroupMembersFSP")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9281e51745650974fda168166a2ed463a34f0714")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e30074b7bcdae5111ce2641d7131bb5f306889d6")]
 [assembly: System.Reflection.AssemblyProductAttribute("GetADGroupMembersFSP")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GetADGroupMembersFSP")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
